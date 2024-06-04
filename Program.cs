797b307ecffdbf4d381cf147d92bf98553219ad9
Gérald Barré <meziantou@users.noreply.github.com>
2024-06-04 11:39:27 -0400

    JWT token improvement (#41)
    * Changed token
    
    Signed-off-by: Paolo Salvatori <paolos@microsoft.com>
    
    * Fixed problem with JWT missing resource and expires_on
    
    Signed-off-by: Paolo Salvatori <paolos@microsoft.com>
    
    ---------
    
    Signed-off-by: Paolo Salvatori <paolos@microsoft.com>
    Co-authored-by: Paolo Salvatori <paolos@microsoft.com>

--- Program.cs
+++ Program.cs
@@ -7,10 +7,19 @@ var app = WebApplication.CreateBuilder(args).Build();
 
 // Can be consumed by ManagedIdentityCredential by specifying IDENTITY_ENDPOINT and IMDS_ENDPOINT environment variables to this action URL
 // See https://github.com/Azure/azure-sdk-for-net/blob/Azure.Identity_1.8.0/sdk/identity/Azure.Identity/src/AzureArcManagedIdentitySource.cs
-app.MapGet("/token", async (string resource) =>
+
+app.MapGet("/token", async (HttpContext context, string resource) =>
 {
     var token = await tokenCredential.GetTokenAsync(new TokenRequestContext(new[] { resource }));
-    return new Dictionary<string, string> { ["access_token"] = token.Token, ["expires_on"] = token.ExpiresOn.ToString("O", CultureInfo.InvariantCulture) };
+    context.Response.Headers.Add("Content-Type", "application/json"); // Set the Content-Type header to JSON
+    return new Dictionary<string, string>
+    {
+        ["access_token"] = token.Token,
+        ["expiresOn"] = token.ExpiresOn.ToString("O", CultureInfo.InvariantCulture),
+        ["expires_on"] = token.ExpiresOn.ToUnixTimeSeconds().ToString(),
+        ["tokenType"] = "Bearer",
+        ["resource"] = resource
+    };
 });
 
 app.Run();
